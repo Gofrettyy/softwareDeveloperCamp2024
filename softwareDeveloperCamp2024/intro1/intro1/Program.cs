30a60e653bd43825e339086d222013acee3993d7
Gofrettyy <naz_1216@outlook.com>
2024-02-05 14:25:01 +0300

    changes added

--- softwareDeveloperCamp2024/intro1/intro1/Program.cs
+++ softwareDeveloperCamp2024/intro1/intro1/Program.cs
@@ -2,3 +2,10 @@
 
 Console.WriteLine("Hello, World!");
 
+string message1 ="Krediler";
+int term = 12;
+double amount =100000;
+
+//variables --> camelCase yazılır.
+bool isAuthenticated = false;
+Console.WriteLine(message1);
