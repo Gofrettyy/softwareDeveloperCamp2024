545baee3cf498dbb0a74651ba4b013b693ef19e4
Gofrettyy <naz_1216@outlook.com>
2024-02-10 00:10:52 +0300

    commit

--- softwareDeveloperCamp2024/intro1/intro1/Entities/Course.cs
+++
@@ -1,9 +0,0 @@
-namespace intro1.Entities;
-
-public class Course
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string Description { get; set; }
-    public double Price { get; set; }   
-}
